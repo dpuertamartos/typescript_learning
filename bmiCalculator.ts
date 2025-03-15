db22b03c36a9c594bd6c2e85c393db89a28e55a6
David Puerta Martos <dpuerta@usal.es>
2025-03-15 12:16:04 +0100

    exercise 9.1

---
+++ bmiCalculator.ts
@@ -0,0 +1,16 @@
+const calculateBmi = (height: number, weight: number): string => {
+    const bmi: number = weight / (height / 100) ** 2;
+
+    switch (true) {
+        case bmi < 18.5:
+            return 'Underweight range';
+        case bmi >= 18.5 && bmi < 25:
+            return 'Normal range';
+        case bmi >= 25 && bmi < 30:
+            return 'Overweight range';
+        case bmi >= 30:
+            return 'Obesity range';
+    }
+}
+
+console.log(calculateBmi(180, 74))
