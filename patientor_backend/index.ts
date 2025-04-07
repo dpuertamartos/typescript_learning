011e4e53a5bb8b2778e21913b34dfc2638d4a2e0
David Puerta Martos <dpuerta@usal.es>
2025-04-07 09:59:50 +0200

    exercise 9.9

--- patientor_backend/index.ts
+++ patientor_backend/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-const PORT = 3000;
+const PORT = 3001;
 const BASE_URI = '/api';
 
 app.get(`${BASE_URI}/ping`, (_req, res) => {
